2a9e9fd1d4c0d489760a4abbfc7faeb87829e20c
JamesDowney <44683462+JamesDowney@users.noreply.github.com>
2021-12-30 13:23:43 -0700

    version 1.0.0

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,81 @@
-import { myMp, print } from "kolmafia";
+import { abort, print, visitUrl } from "kolmafia";
 
-export function checkMP(): string {
-  if (myMp() < 200) {
-    return "Your MP is less than 200.";
-  } else {
-    return "Your MP is greater than or equal to 200.";
+function itemSpade(startIndex: number, endIndex: number) {
+  for (let x = startIndex; x <= endIndex; x++) {
+    const itemPage = visitUrl(`inv_equip.php?pwd=&which=2&action=equip&whichitem=${x}`);
+    if (itemPage.includes("Nopers.")) {
+      print(`Item ${x} does not exist.`);
+    } else {
+      const fleamarketPage = visitUrl(
+        `town_sellflea.php?pwd=&whichitem=${x}&sellprice=&selling=Yep.`
+      );
+      print(
+        fleamarketPage.includes("That item cannot be sold or transferred.")
+          ? `Item ${x} exists but is untradeable.`
+          : `Item ${x} exists and is tradeable.`,
+        `green`
+      );
+    }
   }
 }
 
-export default function main(): void {
-  print(checkMP());
+function skillSpade(startIndex: number, endIndex: number) {
+  for (let x = startIndex; x <= endIndex; x++) {
+    const skillPage = visitUrl(`desc_skill.php?whichskill=${x}}&self=true`);
+    if (skillPage.includes("No skill found.")) {
+      print(`Skill ${x} does not exist.`, `red`);
+    } else {
+      print(`Skill ${x} exists.`, `green`);
+    }
+  }
+}
+
+function familiarSpade(startIndex: number, endIndex: number) {
+  for (let x = startIndex; x <= endIndex; x++) {
+    const familiarPage = visitUrl(`desc_familiar.php?which=${x}`);
+    if (familiarPage.includes("No familiar was found.")) {
+      print(`Familiar ${x} does not exist.`, `red`);
+    } else {
+      print(`Familiar ${x} exists.`, `green`);
+    }
+  }
+}
+
+function outfitSpade(startIndex: number, endIndex: number) {
+  for (let x = startIndex; x <= endIndex; x++) {
+    const skillPage = visitUrl(`desc_outfit.php?whichoutfit=${x}`);
+    if (skillPage.includes("No such outfit is a thing.")) {
+      print(`Outfit ${x} does not exist.`, `red`);
+    } else {
+      print(`Outfit ${x} exists.`, `green`);
+    }
+  }
+}
+
+export function main(argument: string): void {
+  if (argument === undefined) {
+    abort(`Please provide an argument.`);
+  }
+  const command: string[] = argument.split(" ", 3);
+  if (command.length !== 3) {
+    abort('Arguments should be formatted as: "item/skill/familiar/outfit start_index end_index"');
+  }
+  switch (command[0].toLowerCase()) {
+    case "item":
+      itemSpade(+command[1], +command[2]);
+      break;
+    case "skill":
+      skillSpade(+command[1], +command[2]);
+      break;
+    case "familiar":
+    case "fam":
+      familiarSpade(+command[1], +command[2]);
+      break;
+    case "outfit":
+      outfitSpade(+command[1], +command[2]);
+      break;
+    default:
+      print(`Invalid argument.`, `red`);
+      break;
+  }
 }
